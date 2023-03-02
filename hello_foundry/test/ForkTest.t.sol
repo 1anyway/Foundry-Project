8d43aaffa6ef6506ac889d2ea0fcbb9e8880a7c6
周哲 <1708834102@qq.com>
2023-03-02 09:58:06 +0800

    update

---
+++ hello_foundry/test/ForkTest.t.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13
+
+contract ForkTest is Test {
+    uint256 mainnetFork;
+    uint256 optimismFork;
+
+    function setUp() public {
+        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        optimismFork = vm.createFork(OPTIMISM_RPC_URL);
+
+    }
+    
+    function testForkIdDiffer() public {
+        assert(mainnetFork != optimismFork);
+    }
+
+    function testCanSelectFork() public {
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+    }
+
+    function testCanSwitchForks() public {
+        vm.selectFork(mainnetFork);
+        assertEq(vm.activeFork(), mainnetFork);
+
+        vm.selectFork(optimismFork);
+        assertEq(vm.activeFork(), optimismFork);
+    }
+
+    function testCanCreateAndSelectForkInOneStep() public {
+        uint256 anotherFork = vm.createSelectFork(MAINNET_RPC_URL);
+        assertEq(vm.activeFork(), anotherFork);
+    }
+
+    function testCanSetForkBlockNumber() public {
+        vm.selectFork(mainnetFork);
+        vm.rollFork(1_337_000);
+
+        assertEq(block.number, 1_337_000);
+    }
+}
